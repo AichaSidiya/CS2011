e093de5e08a1ac4efa06f7e31a4c9f480ff92522
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2023-06-17 05:14:07 +0300

    Update Java Hashset.java

--- HackerRank/Java Hashset.java
+++ HackerRank/Java Hashset.java
@@ -1,4 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
 
 
 Set<String> mySet = new HashSet<String>();
@@ -11,3 +28,4 @@ for(int i = 0; i < t; i++){
     System.out.println(mySet.size());
 }
 
+    }

ac172cfb16065031f59e5f7b96ded34657598268
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2023-06-17 05:03:57 +0300

    Create Java Subarray.java

---
+++ HackerRank/Java Subarray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = input.nextInt();
+        }
+        int counter = 0;
+        for(int i = 0; i < arr.length; i++){
+            int sum = 0;
+            for(int j = i; j < arr.length; j++){
+                sum += arr[j];
+                if(sum < 0){
+                    counter++;
+                }
+            }
+        }
+        
+        System.out.println(counter);
+    }
+}

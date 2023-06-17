da23a4b087146042af21b037cd1acbb24476b7f2
AichaSidiya <91727165+AichaSidiya@users.noreply.github.com>
2023-06-17 04:56:32 +0300

    Create Java Generics.java

---
+++ HackerRank/Java Generics.java
@@ -0,0 +1,11 @@
+
+
+class Printer
+{
+   public void printArray(Object [] o){
+       for(int i = 0; i < o.length; i++){
+           System.out.println(o[i]);//Write your code here
+       }
+   }
+}
+
